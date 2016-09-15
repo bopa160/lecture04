2e57c4169d6433222954d6ca71707d1d2143b973
bopa160 <thomasciavattone@mail.adelphi.edu>
2016-09-15 11:10:00 -0400

    Update phonenumber.java

--- phonenumber.java
+++ phonenumber.java
@@ -21,7 +21,14 @@ public class PhoneNumber
     //if areaCode is bad, tell user to input something else.
     if (areaCode < 100 || areaCode > 999)
     {
-      System.err.println("Please use a valid number")
+      System.err.println("Please use a valid beginnging number");
+    }
+    else if (numBegin < 100 || numBegin > 999)
+    {
+        System.err.println("Please use a valid middle number");
+    } else if (numEnd <1000 || numEnd > 9999)
+    {
+        System.err.println("Please use a valid end number");
     }
     phoneNumber =  areaCode+ "-" + numBegin + "-"
  + numEnd;
